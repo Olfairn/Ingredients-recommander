2528abb19346f0d9ae9dd208784fd200dc2df052
Olfairn <flo.marandet@gmail.com>
2021-02-19 01:27:14 +0100

    update

--- gettingstarted/app.py
+++ gettingstarted/app.py
@@ -1,28 +1,13 @@
-# pip install flask
-# python -m pip install flask
-
-from flask import Flask, request
-from flask.templating import render_template
-import recommender
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/movie')  # <-- suffix of the URL
-def get_movie():
-    #TODO read URL parameters
-    d = dict(request.args)
-    name = d['movie1']
-    rating = int(d['rating1'])
-    movie = recommender.get_movie_recommendation(name, rating)
-    return render_template('result.html', movie=movie, 
-                           score=0.123,
-                           title='your recommendations:')
 
-@app.route('/')
-def hello():
-    return render_template('main.html') # in templates/
+@app.route('/') 
+@app.route('/index')
+def hello_world():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
+    app.run(host='0.0.0.0')
